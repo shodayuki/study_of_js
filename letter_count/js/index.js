bb436d0580489b2dace9b4a77ddbdfc67ee79ca8
shodayuki <yshoda1227@gmail.com>
2020-06-07 23:50:20 +0900

    文字数カウンター作成

---
+++ letter_count/js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".input").on("keyup", function () {
+    let count = 30 - $(this).val().replace(/\s+/g, '').length;
+    $(".input").text(count);
+
+    if (count < 0) {
+      $(".lest-count").addClass('red');
+    } else {
+      $(".lest-count").removeClass('red');
+    }
+    $(".lest-count").text(count);
+  });
+});
