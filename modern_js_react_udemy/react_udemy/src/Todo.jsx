06f5c1f00d97ace829f1ba6b9f08b108ad1b8ff6
shodayuki <yshoda1227@gmail.com>
2024-03-17 23:34:30 +0900

    タスクの追加機能

--- modern_js_react_udemy/react_udemy/src/Todo.jsx
+++ modern_js_react_udemy/react_udemy/src/Todo.jsx
@@ -2,17 +2,40 @@ import './App.css';
 import {useState} from "react";
 
 export const Todo = () => {
+  // 入力されたTODOのState（todoTextは初期値のState, setTodoTextは更新関数）
+  const [todoText, setTodoText] = useState("");
+
+  // 未完了TODOに関するState(incompleteTodosは初期値のState, setIncompleteTodosは更新関数)
   const [incompleteTodos, setIncompleteTodos] = useState([
       "TODO1です", "TODO2です"
   ]);
+
+  // 完了TODOに関するState（completeTodosは初期値のState, setCompleteTodosは更新関数）
   const [completeTodos, setCompleteTodos] = useState([
       "TODO1でした", "TODO2でした"
   ]);
+
+  // テキスト変更を検知する関数処理
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+      // 空文字の時は何も返却しない処理を実施
+      if (todoText === "") return;
+
+      // スプレッド構文を用いて未完了のTODO配列をコピーして、
+      // newTodos配列にtodoTextを配列に追加
+      const newTodos = [...incompleteTodos, todoText];
+      setIncompleteTodos(newTodos);
+
+      // フォームを空にする
+      setTodoText("");
+  };
+
   return (
     <>
         <div className="input-area">
-            <input type="text" placeholder="TODOを入力" />
-            <button>追加</button>
+            <input type="text" placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+            <button onClick={onClickAdd}>追加</button>
         </div>
         <div className="incomplete-area">
             <p className="title">未完了のTODO</p>
