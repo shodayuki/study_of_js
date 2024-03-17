e85b673edd830471209146d91052429b2017b7b0
shodayuki <yshoda1227@gmail.com>
2024-03-17 20:01:23 +0900

    構造を作成

--- modern_js_react_udemy/react_udemy/src/Todo.jsx
+++ modern_js_react_udemy/react_udemy/src/Todo.jsx
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 export const Todo = () => {
   return (
-    <div>
-    </div>
+    <>
+        <div>
+            <input type="text" placeholder="TODOを入力" />
+            <button>追加</button>
+        </div>
+        <div>
+            <p>未完了のTODO</p>
+            <ul>
+                <li>
+                    <p>TODOです</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                </li>
+                <li>
+                    <p>TODOです</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <p>完了のTODO</p>
+            <ul>
+                <li>
+                    <p>TODOでした</p>
+                    <button>戻す</button>
+                </li>
+                <li>
+                    <p>TODOでした</p>
+                    <button>戻す</button>
+                </li>
+            </ul>
+        </div>
+    </>
   );
-}
+};
 
 export default Todo;
