c5ef90764d0c356baedb6c2ecc737342b0ee184e
shodayuki <yshoda1227@gmail.com>
2024-03-17 22:35:16 +0900

    useStateを用いたマークアップ実装

--- modern_js_react_udemy/react_udemy/src/Todo.jsx
+++ modern_js_react_udemy/react_udemy/src/Todo.jsx
@@ -1,6 +1,13 @@
 import './App.css';
+import {useState} from "react";
 
 export const Todo = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+      "TODO1です", "TODO2です"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState([
+      "TODO1でした", "TODO2でした"
+  ]);
   return (
     <>
         <div className="input-area">
@@ -10,37 +17,32 @@ export const Todo = () => {
         <div className="incomplete-area">
             <p className="title">未完了のTODO</p>
             <ul>
-                <li>
-                    <div className="list-row">
-                        <p className="todo-item">TODOです</p>
-                        <button>完了</button>
-                        <button>削除</button>
-                    </div>
-                </li>
-                <li>
-                    <div className="list-row">
-                        <p className="todo-item">TODOです</p>
-                        <button>完了</button>
-                        <button>削除</button>
-                    </div>
-                </li>
+                {incompleteTodos.map((todo) => {
+                    return (
+                        <li key={todo}>
+                            <div className="list-row">
+                                <p className="todo-item">{todo}</p>
+                                <button>完了</button>
+                                <button>削除</button>
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
         <div className="complete-area">
             <p className="title">完了のTODO</p>
             <ul>
-                <li>
-                    <div className="list-row">
-                        <p className="todo-item">TODOでした</p>
-                        <button>戻す</button>
-                    </div>
-                </li>
-                <li>
-                    <div className="list-row">
-                        <p>TODOでした</p>
-                        <button>戻す</button>
-                    </div>
-                </li>
+                {completeTodos.map((todo) => {
+                    return (
+                        <li key={todo}>
+                            <div className="list-row">
+                                <p className="todo-item">{todo}</p>
+                                <button>戻す</button>
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     </>
