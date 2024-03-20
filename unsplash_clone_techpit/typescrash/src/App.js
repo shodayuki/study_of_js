aa24a9345a0201397d417b7070a84bfdb78b89e8
shodayuki <yshoda1227@gmail.com>
2024-03-20 13:26:44 +0900

    Search.jsx作成

--- unsplash_clone_techpit/typescrash/src/App.js
+++ unsplash_clone_techpit/typescrash/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header>
+          <Search />
+      </Header>
     </div>
   );
 }
---
+++ unsplash_clone_techpit/typescrash/src/components/Search.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import './Search.css';
+
+const Search = () => {
+    return (
+        <form>
+            <input type="text" className='Search'/>
+        </form>
+    );
+};
+
+export default Search;
