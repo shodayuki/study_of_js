91577ebe91f4665115133923aedd1f64eca7b1f7
shodayuki <yshoda1227@gmail.com>
2024-03-20 12:28:37 +0900

    Header.jsx修正

--- unsplash_clone_techpit/typescrash/src/components/Header.jsx
+++ unsplash_clone_techpit/typescrash/src/components/Header.jsx
@@ -1,16 +1,12 @@
 import * as React from "react";
 import './Header.css';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <>
-                <div className="Header">
-                    <span className="Header__Logo">TypeScrash</span>
-                </div>
-            </>
-        );
-    }
+const Header = () => {
+    return (
+        <div className="Header">
+            <span className="Header__Logo">TypeScrash</span>
+        </div>
+    );
 };
 
 export default Header;
