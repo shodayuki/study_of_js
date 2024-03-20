64214c5b48c81bb48dc3405def3a84934c55a8fb
shodayuki <yshoda1227@gmail.com>
2024-03-20 14:15:59 +0900

    画像データを取得

--- unsplash_clone_techpit/typescrash/src/components/Search.jsx
+++ unsplash_clone_techpit/typescrash/src/components/Search.jsx
@@ -1,10 +1,28 @@
 import * as React from "react";
 import './Search.css';
+import { useState } from "react";
 
-const Search = () => {
+const Search = ({ onSearchSubmit }) => {
+    const [term, setTerm] = useState('');
+
+    // エンターキーを押下することによって、formのonSubmitイベントが発火したら実行される関数
+    const onFormSubmit = (event) => {
+        // event処理をキャンセル
+        event.preventDefault();
+
+        // Propsで受け取ったonSearchSubmit関数にStateであるtermを渡して実行
+        onSearchSubmit(term);
+    }
+
     return (
-        <form>
-            <input type="text" className='Search'/>
+        <form onSubmit={onFormSubmit}>
+            <input
+                type="text"
+                className='Search'
+                value={term}
+                placeholder="検索"
+                onChange={e => setTerm(e.target.value)}
+            />
         </form>
     );
 };
