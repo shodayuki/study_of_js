46ba15641496c7c9e699894d871f211b555cdbfa
shodayuki <yshoda1227@gmail.com>
2024-04-15 22:49:50 +0900

    Todo一覧とTodo削除機能を実装

--- brain-react/section_01/src/components/templates/TodoTemplate/index.jsx
+++ brain-react/section_01/src/components/templates/TodoTemplate/index.jsx
@@ -1,13 +1,48 @@
 import {useState} from "react";
-import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../../constants/data";
+import { searchTodo } from "../../../utils/todoLogic";
+import { TodoList } from "../../organisms/TodoList";
+import { INIT_TODO_LIST } from "../../../constants/data";
 import styles from "./styles.module.css";
 
 export const TodoTemplate = () => {
-    // TodoList
+    /* TodoList */
     const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
+    /* Todo採番ID */
+    //const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
+    /* 表示用TodoList */
+    const [showTodoList, setShowTodoList] = useState(INIT_TODO_LIST);
 
-    // Todo採番ID
-    const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
+    /* actions */
+    /**
+     * 表示用Todoリスト更新処理
+     * @param {*} newTodoList
+     * @param {*} keyword
+     */
+    const updateShowTodoList = (newTodoList, keyword) => {
+        setShowTodoList(
+            keyword !== "" ? searchTodo(newTodoList, keyword) : newTodoList
+        );
+    };
+
+    /**
+     * Todo削除処理
+     * @param { number } targetId
+     * @param { string } targetTitle
+     */
+    const handleDeleteTodo = (targetId, targetTitle) => {
+        if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
+            // 削除するid以外のtodoを再編集
+            // filterを用いた方法
+            const newTodoList = originTodoList.filter((todo) => todo.id !== targetId);
+
+            console.log(newTodoList);
+
+            // todoを削除したTodoListの更新
+            setOriginTodoList(newTodoList);
+
+            updateShowTodoList(newTodoList, "");
+        }
+    };
 
     return (
         <div className={styles.container}>
@@ -22,7 +57,12 @@ export const TodoTemplate = () => {
             </section>
             {/* Todoリスト一覧表示 */}
             <section className={styles.common}>
-
+                {showTodoList.length > 0 && (
+                    <TodoList
+                        todoList={showTodoList}
+                        handleDeleteTodo={handleDeleteTodo}
+                    />
+                )}
             </section>
         </div>
     );
