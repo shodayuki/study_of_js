d6e1e202cb239641c65d49a5b6545bb50daa6662
shodayuki <yshoda1227@gmail.com>
2024-04-20 20:23:52 +0900

    削除処理を実装

--- brain-react/section_02/src/components/templates/TodoTemplate/index.jsx
+++ brain-react/section_02/src/components/templates/TodoTemplate/index.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
+import { TodoList } from "../../organisms/TodoList";
 import styles from "./styles.module.css";
+import { INIT_TODO_LIST } from "../../../constants/data";
 
 export const TodoTemplate = () => {
+    /* TodoList */
+    const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
+    /* 表示用TodoList */
+    const [showTodoList, setShowTodoList] = useState(INIT_TODO_LIST);
+
+    /* actions */
+    /**
+     * 表示用Todoリスト更新処理
+     *
+     * @param {*} newTodoList
+     */
+    const updateShowTodoList = (newTodoList) => {
+        setShowTodoList(newTodoList);
+    };
+
+    /**
+     * Todo削除処理
+     *
+     * @param { number } targetId
+     * @param { string } targetTitle
+     */
+    const handleDeleteTodo = (targetId, targetTitle) => {
+        if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
+            const newTodoList = [...originTodoList];
+            const deleteIndex = newTodoList.findIndex((todo) => todo.id === targetId);
+            newTodoList.splice(deleteIndex, 1);
+
+            // todoを削除したTodoListの更新
+            setOriginTodoList(newTodoList);
+
+            updateShowTodoList(newTodoList);
+        }
+    }
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Todo List</h1>
@@ -14,7 +51,12 @@ export const TodoTemplate = () => {
             </section>
             {/* Todoリスト一覧表示 */}
             <section className={styles.common}>
-
+                {showTodoList.length > 0 && (
+                    <TodoList
+                        todoList={showTodoList}
+                        handleDeleteTodo={handleDeleteTodo}
+                    />
+                )}
             </section>
         </div>
     );
