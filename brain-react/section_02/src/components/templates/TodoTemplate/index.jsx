c21629461c6b4cb62ebcf28178e7adacb24ce8bb
shodayuki <yshoda1227@gmail.com>
2024-04-20 18:59:53 +0900

    表示画面を実装

---
+++ brain-react/section_02/src/components/templates/TodoTemplate/index.jsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+
+export const TodoTemplate = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Todo List</h1>
+            {/* Todo追加エリア */}
+            <section className={styles.common}>
+
+            </section>
+            {/* Todo検索フォームエリア */}
+            <section className={styles.common}>
+
+            </section>
+            {/* Todoリスト一覧表示 */}
+            <section className={styles.common}>
+
+            </section>
+        </div>
+    );
+};
--- brain-react/section_02/src/index.js
+++ brain-react/section_02/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { TodoPage } from './pages/todo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoPage />
   </React.StrictMode>
 );
 
