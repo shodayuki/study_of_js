37f0d7d77a642d6c0afd77e8d9c0cd3e13d86fc6
shodayuki <yshoda1227@gmail.com>
2024-04-21 22:26:18 +0900

    context実装

--- brain-react/section_04/src/pages/todo.jsx
+++ brain-react/section_04/src/pages/todo.jsx
@@ -1,3 +1,14 @@
+/**
+ * TodoPage
+ *
+ * @package pages
+ */
+import { TodoProvider } from "../contexts/TodoContext";
 import { TodoTemplate } from "../components/templates/TodoTemplate";
 
-export const TodoPage = () => <TodoTemplate />;
+export const TodoPage = () => (
+    <TodoProvider>
+        {/* TodoProvider直下のコンポーネントでコンテキストに定義した状態、ロジックをどこでも呼び出せる */}
+        <TodoTemplate />
+    </TodoProvider>
+);
