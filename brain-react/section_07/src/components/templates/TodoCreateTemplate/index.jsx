2fb0ca4754b18cc939c758219980344501775763
shodayuki <yshoda1227@gmail.com>
2024-04-27 11:56:01 +0900

    CommonButtonコンポーネント作成

--- brain-react/section_07/src/components/templates/TodoCreateTemplate/index.jsx
+++ brain-react/section_07/src/components/templates/TodoCreateTemplate/index.jsx
@@ -3,9 +3,11 @@
  *
  * @package components
  */
+import { useState, useCallback } from "react";
 import { BaseLayout } from "../../organisims/BaseLayout";
 import { InputForm } from "../../atoms/InputForm";
 import { TextArea } from "../../atoms/TextArea";
+import { CommonButton } from "../../atoms/CommonButton";
 import styles from "./styles.module.css";
 
 /**
@@ -15,16 +17,57 @@ import styles from "./styles.module.css";
  * @constructor
  */
 export const TodoCreateTemplate = () => {
+    /* local state */
+    const [inputTitle, setInputTitle] = useState("");
+    const [inputContent, setInputContent] = useState("");
+
+    /**
+     * 「title」の変更処理
+     *
+     * @type {function(*): void}
+     */
+    const handleChangeTitle = useCallback(
+        (e) => setInputTitle(e.target.value), []
+    );
+
+    /**
+     * 「content」変更処理
+     *
+     * @type {function(*): void}
+     */
+    const handleChangeContent = useCallback(
+        (e) => setInputContent(e.target.value), []
+    );
+
+    const handleCreateTodo = useCallback((e) => {
+        e.preventDefault();
+        console.log("Hello");
+    }, []);
+
     return (
         <BaseLayout title={"Create Todo"}>
-            <div className={styles.container}>
+            <form className={styles.container} onClick={handleCreateTodo}>
                 <div className={styles.area}>
-                    <InputForm placeholder={"Title"} />
+                    <InputForm
+                        value={inputTitle}
+                        placeholder={"Title"}
+                        onChange={handleChangeTitle}
+                    />
                 </div>
                 <div className={styles.area}>
-                    <TextArea placeholder={"Content"} />
+                    <TextArea
+                        value={inputContent}
+                        placeholder={"Content"}
+                        onChange={handleChangeContent}
+                    />
                 </div>
-            </div>
+                <div className={styles.area}>
+                    <CommonButton
+                        type="submit"
+                        label="Create Todo"
+                    />
+                </div>
+            </form>
         </BaseLayout>
     );
 };
