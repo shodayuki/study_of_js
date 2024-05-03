ab14e24a678c583cdfdc557dcfbd13833d674f92
shodayuki <yshoda1227@gmail.com>
2024-05-03 19:53:42 +0900

    useTodoCreateTemplate作成

--- brain-react/section_08/src/components/templates/TodoCreateTemplate/index.jsx
+++ brain-react/section_08/src/components/templates/TodoCreateTemplate/index.jsx
@@ -3,14 +3,13 @@
  *
  * @package components
  */
-import React, { useState, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { BaseLayout } from '../../organisims/BaseLayout';
 import { InputForm } from '../../atoms/InputForm';
 import { TextArea } from '../../atoms/TextArea';
 import { CommonButton } from '../../atoms/CommonButton';
 import { useTodoContext } from '../../../contexts/TodoContext';
-import { NAVIGATION_LIST } from '../../../constants/navigations';
+import { useTopCreateTemplate } from './useTopCreateTemplate';
 import styles from './styles.module.css';
 
 /**
@@ -20,46 +19,12 @@ import styles from './styles.module.css';
  * @constructor
  */
 export const TodoCreateTemplate = () => {
-  const navigate = useNavigate();
   const { addTodo } = useTodoContext();
 
-  /* local state */
-  const [inputTitle, setInputTitle] = useState("");
-  const [inputContent, setInputContent] = useState("");
-
-  /**
-   * titleの変更処理
-   *
-   * @type {function(*): void}
-   */
-  const handleChangeTitle = useCallback(
-    (e) => setInputTitle(e.target.value), []
-  );
-
-  /**
-   * contentの変更処理
-   *
-   * @type {function(*): void}
-   */
-  const handleChangeContent = useCallback(
-    (e) => setInputContent(e.target.value), []
-  );
-
-  /**
-   * Todo追加処理
-   *
-   * @type {(function(*): void)|*}
-   */
-  const handleCreateTodo = useCallback(
-    (e) => {
-      e.preventDefault();
-      if (inputTitle !== "" && inputContent !== "") {
-        addTodo(inputTitle, inputContent);
-        navigate(NAVIGATION_LIST.TOP);
-      }
-    },
-    [addTodo, inputTitle, inputContent, navigate]
-  );
+  const [
+    { inputTitle, inputContent },
+    { handleChangeTitle, handleChangeContent, handleCreateTodo }
+  ] = useTopCreateTemplate({ addTodo });
 
   return (
     <BaseLayout title={"Create Todo"}>
