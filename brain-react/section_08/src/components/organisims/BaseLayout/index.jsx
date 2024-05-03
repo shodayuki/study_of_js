45baab11853d6ad5dc5f63bd3197e084961ab75e
shodayuki <yshoda1227@gmail.com>
2024-05-03 14:52:00 +0900

    BaseLayout作成

---
+++ brain-react/section_08/src/components/organisims/BaseLayout/index.jsx
@@ -0,0 +1,29 @@
+/**
+ * BaseLayout
+ *
+ * @package components
+ */
+import React from 'react';
+import { Navigation } from '../../molecules/Navigation';
+import styles from "./styles.module.css";
+
+/**
+ * BaseLayout
+ *
+ * @param children
+ * @param title
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const BaseLayout = ({ children, title }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      {/* リンクエリア */}
+      <section className={styles.common}>
+        <Navigation />
+      </section>
+      {children}
+    </div>
+  );
+};
