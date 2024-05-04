36515356dc11e32c878c315ea6c11cec585bb6f1
shodayuki <yshoda1227@gmail.com>
2024-05-04 11:52:57 +0900

    コンポーネントをリファクタリング

--- brain-react/section_08/src/components/atoms/InputForm/index.jsx
+++ brain-react/section_08/src/components/atoms/InputForm/index.jsx
@@ -12,19 +12,20 @@ import styles from "./style.module.css";
  * @param {*} props
  * @returns
  */
-export const InputForm = (props) => {
-  /* props */
-  const { disabled = false, value, placeholder, onChange, onKeyDown } = props;
-
-  return (
-    <input
-      disabled={disabled}
-      className={styles.input}
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-    />
-  );
-}
+export const InputForm = ({
+  disabled = false,
+  value,
+  placeholder,
+  onChange,
+  onKeyDown
+}) => (
+  <input
+    disabled={disabled}
+    className={styles.input}
+    type="text"
+    placeholder={placeholder}
+    value={value}
+    onChange={onChange}
+    onKeyDown={onKeyDown}
+  />
+);
