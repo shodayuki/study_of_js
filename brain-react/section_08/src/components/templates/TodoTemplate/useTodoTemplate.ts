9b484ba99454d1ac812a9e20dbc3b1d361aa0146
shodayuki <yshoda1227@gmail.com>
2024-05-05 11:54:06 +0900

    TodoTemplate components

--- brain-react/section_08/src/components/templates/TodoTemplate/useTodoTemplate.ts
+++ brain-react/section_08/src/components/templates/TodoTemplate/useTodoTemplate.ts
@@ -4,13 +4,28 @@
  * @package hooks
  */
 import { useMemo, useState, useCallback } from 'react';
+import { TodoType } from '../../../interfaces/Todo';
+import { EventType } from '../../../interfaces/Event';
+
+type Params = {
+  originTodoList: Array<TodoType>
+}
+
+type StatesType = {
+  searchKeyword: string,
+  showTodoList: Array<TodoType>
+}
+
+type ActionsType = {
+  handleChangeSearchKeyword: EventType['onChangeInput']
+}
 
 /**
  * useTodoTemplate
  *
  * @param originTodoList
  */
-export const useTodoTemplate = ({ originTodoList }) => {
+export const useTodoTemplate = ({ originTodoList }: Params) => {
   /* 検索キーワード */
   const [searchKeyword, setSearchKeyword] = useState("");
 
@@ -29,18 +44,18 @@ export const useTodoTemplate = ({ originTodoList }) => {
    * @type {function(*): void}
    * @param {*} e
    */
-  const handleChangeSearchKeyword = useCallback(
+  const handleChangeSearchKeyword: EventType['onChangeInput'] = useCallback(
     (e) => setSearchKeyword(e.target.value),[]
   );
 
-  const states = {
+  const states: StatesType = {
     searchKeyword,
     showTodoList
   };
 
-  const actions = {
+  const actions: ActionsType = {
     handleChangeSearchKeyword
   };
 
-  return [states, actions];
+  return [states, actions] as const;
 };
