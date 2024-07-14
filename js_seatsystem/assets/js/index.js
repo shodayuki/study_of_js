4f2ae62113d91eb4d05d1374ee5224a6364d1ed2
shodayuki <yshoda1227@gmail.com>
2024-07-14 14:11:28 +0900

    乱数を用いた繰り返し処理を追加

--- js_seatsystem/assets/js/index.js
+++ js_seatsystem/assets/js/index.js
@@ -1,6 +1,13 @@
 const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 let insertHTML = '';
 
+for (let i = studentNumbers.length; i > 0; i--) {
+  const randomNum = Math.floor(Math.random() * i);
+  let tmp = studentNumbers[i - 1];
+  studentNumbers[i - 1] = studentNumbers[randomNum];
+  studentNumbers[randomNum] = tmp;
+}
+
 studentNumbers.forEach(function (num) {
   insertHTML += '<div class="seat__item">' + num + '</div>';
 })
