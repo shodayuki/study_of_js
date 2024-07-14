362d3cc1d82ae00809ef4c78daa05d3f3ad97752
shodayuki <yshoda1227@gmail.com>
2024-07-14 23:00:54 +0900

    入出金のデータを表示する処理を実装

--- moneyforward_clone_js/indexedDB.js
+++ moneyforward_clone_js/indexedDB.js
@@ -54,6 +54,9 @@ function regist() {
 
   // データベースにデータを登録
   insertData(balance, date, category, amount, memo);
+
+  // 入出金一覧を作成
+  createList();
 }
 
 // データの挿入
@@ -106,5 +109,53 @@ function insertData(balance, date, category, amount, memo) {
     }
 
     db.close();
+  }
+}
+
+function createList() {
+  // データベースからデータを全件取得
+  let database = indexedDB.open(dbName);
+  database.onsuccess = function(event) {
+    let db = event.target.result;
+    let transaction = db.transaction(storeName, "readonly");
+    let store = transaction.objectStore(storeName);
+
+    store.getAll().onsuccess = function(data) {
+      console.log(data);
+      let rows = data.target.result;
+      let section = document.getElementById("list");
+
+      // 入出金一覧のテーブルを作る
+      // バッククオートでヒアドキュメント
+      let table = `
+				<table>
+					<tr>
+						<th>日付</th>
+						<th>収支</th>
+						<th>カテゴリ</th>
+						<th>金額</th>
+						<th>メモ</th>
+						<th>削除</th>
+					</tr>
+				</table>
+			`;
+
+      // 入出金のデータを表示
+      rows.forEach((element) => {
+        console.log(element);
+        table += `
+					<tr>
+						<td>${element.date}</td>
+						<td>${element.balance}</td>
+						<td>${element.category}</td>
+						<td>${element.amount}</td>
+						<td>${element.memo}</td>
+						<td><button>×</button></td>
+					</tr>
+				`;
+      });
+      table += `</table>`;
+      section.innerHTML = table;
+    }
   }
 }
