49b3ed7bb2bf24a0db6c30f3ae748b5c9a39e64c
shodayuki <yshoda1227@gmail.com>
2024-07-15 11:15:53 +0900

    問題数を増加

--- typing_game/js/index.js
+++ typing_game/js/index.js
@@ -4,13 +4,14 @@ $(function(){
   const $yomi = $('#yomi');
   const $mondai = $('#mondai');
   const $finishPanel = $('#finish-panel');
+  const $countSelect = $('#count-select');
 
   // 問題用の変数の初期化
   let char_index = 1;
 
   // 最初の問題
   let max_length = 3;
-  let question_number = 0;
+  let question_number = 1;
   let question_limit = 3;
 
   // 問題
@@ -20,7 +21,20 @@ $(function(){
     { yomi: 'サイフ', text: 'saifu' },
     { yomi: 'バナナ', text: 'banana' },
     { yomi: 'くつした', text: 'kutsushita' },
+    { yomi:'なべ', text:'nabe' },
+    { yomi:'あし', text:'ashi' },
+    { yomi:'パソコン', text:'pasokon' },
+    { yomi:'けいたい', text:'keitai' },
+    { yomi:'ふとん', text:'futon' },
   ];
+
+  // 最初の問題の設定
+  changeQuestionWord(0);
+
+  $countSelect.on('change', function (e) {
+    question_limit = Number($countSelect.val());
+    changeQuestionWord(0);
+  });
 
   $(document).on('keypress', function(e){
     // alert('key:'+e.key);
@@ -44,7 +58,7 @@ $(function(){
         finish();
         return;
       }
-      changeQuestionWord();
+      changeQuestionWord(question_number);
       char_index = 1; // 初期化
     }
   });
@@ -56,8 +70,8 @@ $(function(){
   }
 
   // 次の問題の実装
-  function changeQuestionWord() {
-    const word = MONDAI_LIST[question_number]['text'];
+  function changeQuestionWord(index) {
+    const word = MONDAI_LIST[index]['text'];
     max_length = word.length;
     let newHtml = '';
     for (let i = 0; i < max_length; i++){
@@ -65,6 +79,6 @@ $(function(){
     }
     // console.log('newHtml: ' + newHtml);
     $mondai.html(newHtml);
-    $yomi.text(MONDAI_LIST[question_number]['yomi']);
+    $yomi.text(MONDAI_LIST[index]['yomi']);
   }
 });
